0086ffc3678ffc26b780c7dc3f55d9f48564fccd
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-11-29 11:39:24 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -20,15 +20,14 @@ echo "Saved client.jar to \"$TEMP_DOWNLOAD_DIR\"."
 echo "Extract assets from client.jar"
 mkdir -p "$TEMP_DOWNLOAD_DIR/generated"
         
-unzip $TEMP_DOWNLOAD_DIR/client.jar "pack.png" -d "$TEMP_DOWNLOAD_DIR/generated"
-unzip $TEMP_DOWNLOAD_DIR/client.jar "assets/*" -d "$TEMP_DOWNLOAD_DIR/generated"
+unzip $TEMP_DOWNLOAD_DIR/client.jar "pack.png" -d "$INPUT_PATH"
+unzip $TEMP_DOWNLOAD_DIR/client.jar "assets/*" -d "$INPUT_PATH/assets"
 
 
 echo "Fetch asset index URL from \"$package_url\""
 asset_index_url=$(curl -L $package_url | jq -r ".assetIndex.url")
 
-assets_path="$TEMP_DOWNLOAD_DIR/generated/assets"
-mkdir -p "$assets_path"
+assets_path="$INPUT_PATH/assets"
 
 curl -L "$asset_index_url" | jq -r '.objects | to_entries[] | "\(.key) \(.value.hash)"' | while read -r path hash; do
   
@@ -44,11 +43,5 @@ curl -L "$asset_index_url" | jq -r '.objects | to_entries[] | "\(.key) \(.value.
 
   echo "Saved \"$url\" to \"$destination\"."
 done
-
-echo "Copying generated files to $INPUT_PATH"
-mkdir -p "$INPUT_PATH"
-cp -r "$TEMP_DOWNLOAD_DIR/generated/"* "$INPUT_PATH"
-
-echo "Files copied to $INPUT_PATH"
 
 exit 0
