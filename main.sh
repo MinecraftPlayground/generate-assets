6e7d0a32fc2343c54c11b0a27929bba599a8703c
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-11-29 10:58:33 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -22,7 +22,6 @@ unzip $TEMP_DOWNLOAD_DIR/client.jar "pack.png" -d "$TEMP_DOWNLOAD_DIR/generated"
 unzip $TEMP_DOWNLOAD_DIR/client.jar "assets/*" -d "$TEMP_DOWNLOAD_DIR/generated"
 
 
-
 echo "Fetch asset index URL from \"$package_url\""
 asset_index_url=$(curl -L $package_url | jq -r ".assetIndex.url")
 
@@ -30,7 +29,7 @@ assets_path=$(mkdir -p "$TEMP_DOWNLOAD_DIR/generated/assets")
 
 curl -L $asset_index_url | jq -r '.objects | to_entries[] | "\(.key) \(.value.hash)"' | while read -r path hash; do
   
-  mkdir -p "$assets_path/$(dirname "$PATH")"
+  mkdir -p "$assets_path/$(dirname $PATH)"
   
   first_hex="${hash:0:2}"
 
@@ -44,3 +43,5 @@ curl -L $asset_index_url | jq -r '.objects | to_entries[] | "\(.key) \(.value.ha
 done
 
 ls -al $TEMP_DOWNLOAD_DIR
+
+exit 0
